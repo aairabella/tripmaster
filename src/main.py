fd410cd065f24415990698c2e803773fdb63f7ba
Andrés Miguel Airabella <a.airabella@gmail.com>
2020-04-11 15:03:22 -0300

    cada uno en su cuadrado

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from machine import Pin, SPI
 import st7920
-spi = SPI(1)
-screen = st7920.Screen(sck=Pin(14), mosi=Pin(13), miso=Pin(12), spi=1, slaveSelectPin=Pin(15), resetDisplayPin=Pin(5))
-screen.plot(5, 5)
-screen.line(10, 10, 15, 15)
-screen.rect(20, 20, 25, 25)
-screen.fill_rect(30, 30, 40, 40)
-screen.fill_rect(32, 32, 38, 38, False)
+
+screen = st7920.Screen(sck=Pin(14), mosi=Pin(13), miso=Pin(12), resetDisplayPin=Pin(15), baudrate=900000)
+
+screen.clear()
+
+screen.plot(10, 12)
+screen.fill_rect(10,12, 50, 52)
+
 screen.redraw()
